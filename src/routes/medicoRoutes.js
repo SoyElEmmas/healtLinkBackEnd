d34dc1f90bd7300f61c65f611d6cb76a043a9102
DanyaSantos <santosresendezde1720@gmail.com>
2023-07-16 15:43:26 -0600

    crud de medico

--- src/routes/medicoRoutes.js
+++ src/routes/medicoRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getMedicos} = require('../controllers/medicoControllers.js')
+const {getMedicos,createMedico,updateMedico,deleteMedico} = require('../controllers/medicoControllers.js')
 
 router.get('/', getMedicos)
+router.post('/',createMedico)
+router.put('/:id',updateMedico)
+router.delete('/:id',deleteMedico)
 
 
 
